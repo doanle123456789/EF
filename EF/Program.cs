7fd69c133a4e22641b6e40649ab73125e9affa3e
doanle123456789 <doanngocle17012016@gmail.com>
2021-06-16 10:21:18 +0700

    Add project files.

---
+++ EF/Program.cs
@@ -0,0 +1,14 @@
+﻿using EF.Models;
+using System;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new ProductDbContext();
+
+        }
+    }
+}
